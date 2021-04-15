c05c31246bfe11702c8ac947d9c1ac6423a8c822
shinjein <shinjein@hu-berlin.de>
2021-04-15 14:30:40 +0100

    iterations 1-6 completed

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const mongoose = require('mongoose');
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
 const data = require('./data');
+const { insertMany } = require('./models/Recipe.model');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
+
 // Connection to the database "recipe-app"
 mongoose
-  .connect(MONGODB_URI, {
+ .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -25,3 +27,34 @@ mongoose
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+  //iteration 2
+  Recipe.create({
+    title: 'Cereal',
+    level: 'Easy Peasy',
+    ingredients: ['musli', 'milk'],
+    cuisine: 'Global',
+    dishType: 'breakfast',
+    duration: 2,
+    creator: 'anyone',
+  }).then(() => {
+    console.log('recipe added')
+  });
+
+  Recipe.insertMany(data)
+  .then((data) => data.forEach((recipe) => console.log(recipe.title)))
+  .catch((err) => console.log(err));
+
+  mongoose.set('useFindAndModify', false);
+
+  Recipe.findOneAndUpdate({name: 'Carrot Cake'}, {level: 'Chef'})
+  .then(() => {
+    console.log('recipe updated');
+  });
+
+  Recipe.deleteOne({name: 'Carrot Cake'})
+    .then(() => {
+      console.log('carrot cake deleted');
+
+        mongoose.connection.close();
+    });
